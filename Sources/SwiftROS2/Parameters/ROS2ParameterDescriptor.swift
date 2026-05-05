da2c0354109bb83a520c58e78cffdb044e0bca26
Yutaka Kondo <yutaka.kondo@youtalk.jp>
2026-05-05 15:06:20 -0700

    feat(api): add ROS2ParameterDescriptor

---
+++ Sources/SwiftROS2/Parameters/ROS2ParameterDescriptor.swift
@@ -0,0 +1,36 @@
+public struct ROS2ParameterDescriptor: Sendable, Equatable {
+    public var name: String
+    public var type: ROS2ParameterType
+    public var description: String
+    public var additionalConstraints: String
+    public var readOnly: Bool
+    public var dynamicTyping: Bool
+    public var floatingPointRange: ClosedRange<Double>?
+    public var floatingPointStep: Double?
+    public var integerRange: ClosedRange<Int64>?
+    public var integerStep: Int64?
+
+    public init(
+        name: String = "",
+        type: ROS2ParameterType = .notSet,
+        description: String = "",
+        additionalConstraints: String = "",
+        readOnly: Bool = false,
+        dynamicTyping: Bool = false,
+        floatingPointRange: ClosedRange<Double>? = nil,
+        floatingPointStep: Double? = nil,
+        integerRange: ClosedRange<Int64>? = nil,
+        integerStep: Int64? = nil
+    ) {
+        self.name = name
+        self.type = type
+        self.description = description
+        self.additionalConstraints = additionalConstraints
+        self.readOnly = readOnly
+        self.dynamicTyping = dynamicTyping
+        self.floatingPointRange = floatingPointRange
+        self.floatingPointStep = floatingPointStep
+        self.integerRange = integerRange
+        self.integerStep = integerStep
+    }
+}
